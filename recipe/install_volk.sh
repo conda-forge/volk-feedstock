2c40e30cc2c60f145e2da4d18c6f9f80977b0489
Ryan Volz <ryan.volz@gmail.com>
2021-05-06 17:05:25 -0400

    Install list_cpu_features as list_cpu_features_volk as user aid.

--- recipe/install_volk.sh
+++ recipe/install_volk.sh
@@ -7,3 +7,7 @@ cmake -DCMAKE_INSTALL_LOCAL_ONLY=1 -P cmake_install.cmake
 cmake -P apps/cmake_install.cmake
 
 cmake -P lib/cmake_install.cmake
+
+# install list_cpu_features manually so we can rename it specific to volk
+# (it is statically linked to cpu_features, so don't have to install the lib)
+cmake -E copy cpu_features/list_cpu_features "$PREFIX/bin/list_cpu_features_volk"
