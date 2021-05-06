24b479067d6dc4aa7775c17fd195477a4d023868
Ryan Volz <ryan.volz@gmail.com>
2021-05-05 21:30:46 -0400

    Add patch and make volk_modtool command into an entry point.

--- recipe/install_volk_modtool.bat
+++ recipe/install_volk_modtool.bat
@@ -5,10 +5,10 @@ cd build
 cmake -P python/volk_modtool/cmake_install.cmake
 if errorlevel 1 exit 1
 
-:: delete the mangled volk_modtool script and install it fresh from the source
-:: to the Scripts directory so it is found and installed correctly as noarch Python
+:: delete volk_modtool script and instead add the source to the module so it
+:: can be used for module invocation and as an entry_point
 del "%LIBRARY_PREFIX%\bin\volk_modtool.py"
 if errorlevel 1 exit 1
 
-xcopy "%SRC_DIR%\python\volk_modtool\volk_modtool" "%PREFIX%\Scripts\"
+xcopy "%SRC_DIR%\python\volk_modtool\volk_modtool" "%LIBRARY_PREFIX%\site-packages\volk_modtool\__main__.py*"
 if errorlevel 1 exit 1
--- recipe/install_volk_modtool.sh
+++ recipe/install_volk_modtool.sh
@@ -4,6 +4,7 @@ cd build
 
 cmake -P python/volk_modtool/cmake_install.cmake
 
-# delete the mangled volk_modtool script and install it fresh from the source
+# delete volk_modtool script and instead add the source to the module so it
+# can be used for module invocation and as an entry_point
 rm -f "$PREFIX/bin/volk_modtool"
-cp "$SRC_DIR/python/volk_modtool/volk_modtool" "$PREFIX/bin/volk_modtool"
+cp "$SRC_DIR/python/volk_modtool/volk_modtool" "$PREFIX/site-packages/volk_modtool/__main__.py"
