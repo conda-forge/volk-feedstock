dd29101881f36798f4e1c3819d6d21c2e447536e
Ryan Volz <ryan.volz@gmail.com>
2023-12-05 23:03:46 -0500

    Update to new default of not installing list_cpu_features with volk.

--- recipe/install_volk.bat
+++ recipe/install_volk.bat
@@ -10,7 +10,3 @@ if errorlevel 1 exit 1
 
 cmake -P lib/cmake_install.cmake
 if errorlevel 1 exit 1
-
-:: install list_cpu_features manually so we can rename it specific to volk
-:: (it is statically linked to cpu_features, so don't have to install the lib)
-cmake -E copy cpu_features/list_cpu_features.exe "%LIBRARY_BIN%/list_cpu_features_volk.exe"
